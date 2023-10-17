cf77633a2c4b8bd90aba68a6e2402b0533468085
Belcsik Bence <belcsik.bebe09@gmail.com>
2023-10-17 23:29:48 +0200

    modify LeaderboardResponse file

--- project_java/src/main/java/hu/bme/aut/onlab/dxhjoh/couchsurfing_java_backend/response/LeaderboardResponse.java
+++ project_java/src/main/java/hu/bme/aut/onlab/dxhjoh/couchsurfing_java_backend/response/LeaderboardResponse.java
@@ -2,12 +2,11 @@ package hu.bme.aut.onlab.dxhjoh.couchsurfing_java_backend.response;
 
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 public class LeaderboardResponse {
 
     private int id;
+    private int userId;
     private String fullName;
     private String username;
     private int score;
